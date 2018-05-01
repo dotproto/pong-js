4d022408f06fb4b73cec19a85d0ede934fe96fa1
Simeon Vincent <svincent@gmail.com>
2018-04-30 21:11:56 -0700

    Rough sketch of core game types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,41 @@
-class Main {
-  constructor() {
-    console.log('test');
-  }
+// ESM syntax is supported.
+
+interface PlayerState {
+  position: number;
 }
 
-new Main();
+interface GameHistory {
+  player1: PlayerState;
+  player2: PlayerState;
+}
+
+interface GameBoard {
+  height: number;
+  width: number;
+}
+
+abstract class GameState {
+  volley: number;
+
+  /** Size of the ball. Effects bounce calculation & rendering properties. */
+  ballSize: number;
+  ballX: number;
+  ballY: number;
+}
+
+interface GameConfig {
+  board: GameBoard;
+  velocity: number;
+  keepHistory: boolean;
+  volley: number;
+}
+
+export class PongEngine {
+  config: GameConfig;
+  history: GameHistory[];
+
+  constructor (config: GameConfig, history: GameHistory[] = []) {
+    this.config = config;
+    this.history = history;
+  }
+}
