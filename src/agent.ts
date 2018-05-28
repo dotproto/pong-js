0c8aa8520a250753ce3a21c235df82d7c82c331e
honzo0481 <gonzalesre@gmail.com>
2018-05-27 17:54:37 -0700

    wip

--- src/agent.ts
+++ src/agent.ts
@@ -1,5 +1,4 @@
 import * as tf from '@tensorflow/tfjs'
-
 enum Player {
   P1 = 'p1',
   P2 = 'p2',
@@ -11,33 +10,43 @@ enum Move {
 }
 
 export default class PGAgent {
+  /** Hyperparameters */
+  /** Learning rate - */
+  alpha = 1e-4;
+  /** Discount factor - */
+  gamma = 0.99;
+  /** size of the policy network's hidden layer */
+  hidden_units = 50;
 
-  hidden_layer_neurons = 200
-  batch_size = 10
-  learning_rate = 1e-4
-  gamma = 0.99
-  decay_rate = 0.99
-  render = false
-
-  model = tf.sequential();
-
-  actions = []
+  /** Policy network - outputs the probability of an action given the current state */
+  policy_net = tf.sequential();
+  /** hidden layer of the policy net */
+  //  TODOs:
+  //    h.config.inputShape = State.shape
+  //    State.shape = paddleLen + paddlePos*2 + ballPos(x, y) + ballV, + ballRad
+  hidden = tf.layers.dense({units: this.hidden_units, activation: 'relu', inputShape: [6]});
+  /** outputs the best action for the observed prior state */
+  // need to switch to softmax to handle num actions > 2 
+  best_action = tf.layers.dense({units: 1, activation: 'sigmoid'});
+  /**  */
+  loss = tf.losses.logLoss(labels=tf.variable, predictions=tf.variable, weights=tf.variable);
 
   constructor (actions:Move[]) {
-    /** fully connected nn. */
-    this.model.add(
-      tf.layers.dense({
-        units: this.hidden_layer_neurons,
-        activation: 'relu',
-        inputShape: [1]
-      })
-    )
-    this.model.add(
-      tf.layers.dense({
-        units: this.
-      })
-    )
-    /** training procedure. */
+    /** build and compile the policy network */
+    this.policy_net.add(this.hidden);
+    this.policy_net.add(this.best_action);
 
+    this.policy_net.compile({loss: this.loss, optimizer: tf.train.adam(this.alpha)});
+  }
+
+  discount(r, gamma) {
+    let discounted_reward = tf.zerosLike(r);
+    let G = 0.0;
+    /** loop from 0 to r.size 
+     *  if r[t] != 0 then set run = 0
+     *  G = G * gamma + r[t]
+     *  discounted_reward[t] = G
+    */
+    return discounted_reward;
   }
 }
